40eb74b0d3c90e8c085a92ae4330473c444a181a
DAC <dac@cogsmith.com>
2021-03-17 17:08:49 -0400

    .

--- dev/run.cmd
+++ dev/run.cmd
@@ -2,7 +2,7 @@ FOR /F "tokens=4 delims= " %%i in ('route print ^| find " 0.0.0.0"') do SET MYIP
 SET HIVE=DEMO
 SET HIVEPATH=W:/DEV/HIVE
 SET CODEPATH=W:/DEV/CODE/zx-hive
-docker rmi --force cogsmith/helloworld-nodejs ; docker rmi --force cogsmith/nodeinfo ; docker rmi --force cogsmith/wx-static ; docker rmi --force cogsmith/zx-hive ; docker rmi --force cogsmith/zx-proxy
+docker rmi --force cogsmith/helloworld-nodejs cogsmith/nodeinfo cogsmith/wx-static cogsmith/zx-hive cogsmith/zx-proxy
 docker build -t cogsmith/zx-hive %CODEPATH%
 docker stop ZXHIVE_%HIVE%
 docker rm ZXHIVE_%HIVE%
