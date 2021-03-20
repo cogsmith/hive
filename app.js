fbcfe57519a9f84d496253258061af34ed4cee35
DAC <dac@cogsmith.com>
2021-03-20 07:51:40 -0400

    .

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ App.Load = function (cell) {
 	fs.writeFileSync('/hive/WWW/.well-known/acme-challenge/acme.txt', 'ACME');
 
 	let adminips = ''; for (let i = 0; i < App.AdminIP.length; i++) { let ip = App.AdminIP[i]; if (ip) { adminips += '--admin ' + ip + ' ' }; }
-	let cmd = "docker stop ZXPROXY_" + App.Hive + " ; docker rm ZXPROXY_" + App.Hive + " ; docker run --rm -t --name ZXPROXY_" + App.Hive + " -p " + App.HiveBind + ":80:80 -p " + App.HiveBind + ":443:443 -v " + App.HivePath + "/" + App.Hive + ":/webgate cogsmith/zx-proxy " + adminips + " --public " + App.HiveIP + " --private " + App.HiveBind + " --to " + App.HiveBind + ' --loglevel trace';
+	let cmd = "docker stop ZXPROXY_" + App.Hive + " ; docker rm ZXPROXY_" + App.Hive + " ; docker run --rm -t --name ZXPROXY_" + App.Hive + " -p " + App.HiveBind + ":80:80 -p " + App.HiveBind + ":443:443 -v " + App.HivePath + "/" + App.Hive + ":/webgate cogsmith/hive-proxy " + adminips + " --public " + App.HiveIP + " --private " + App.HiveBind + " --to " + App.HiveBind + ' --loglevel trace';
 	console.log(cmd);
 	execa.command(cmd, { shell: true }).stdout.pipe(process.stdout);
 
--- dev/run.cmd
+++ dev/run.cmd
@@ -1,9 +1,15 @@
 FOR /F "tokens=4 delims= " %%i in ('route print ^| find " 0.0.0.0"') do SET MYIP=%%i
+SET MYIP=127.0.0.1
 SET HIVE=DEMO
 SET HIVEPATH=W:/DEV/HIVE
 SET CODEPATH=W:/DEV/CODE/zx-hive
-docker rmi --force cogsmith/helloworld-nodejs cogsmith/nodeinfo cogsmith/wx-static cogsmith/zx-hive cogsmith/zx-proxy
+docker rmi --force cogsmith/helloworld-nodejs cogsmith/nodeinfo cogsmith/wx-static cogsmith/zx-hive cogsmith/hive-proxy
 docker build -t cogsmith/zx-hive %CODEPATH%
 docker stop ZXHIVE_%HIVE%
 docker rm ZXHIVE_%HIVE%
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v %HIVEPATH%/%HIVE%:/hive --name ZXHIVE_%HIVE% cogsmith/zx-hive --hive %HIVE% --hivepath %HIVEPATH% --cell ALL --hivebind %MYIP%
+
+
+
+
+
