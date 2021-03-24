7c5a33ca465c0c600b7ca6781cd9bccd090fd83e
DAC <dac@cogsmith.com>
2021-03-24 07:44:40 -0400

    .

--- app.js
+++ app.js
@@ -160,6 +160,7 @@ App.LoadCell = function (cell) {
 		let z = App.CellDB[k];
 		k = k.replace('/web/raw/@', '').replace('/web/raw/_', '/_').replace('/web/raw/', '/').replace('/web/app/@', '').replace('/web/app/_', '/_').replace('/web/app/', '/');
 		let kk = App.GetSlugHost(k.toLowerCase());
+		console.log('K = ' + k + '  ||  ' + 'KK = ' + kk + ' || ' + 'MAPKEY = ' + mapkey);
 		if (kk.startsWith('.')) { kk = kk.substr(1) + '_/*'; }
 		// map[kk] = (!k.substr(-1) == '!' ? '@' : '') + 'http://' + App.HiveBind + ':' + z.Port;
 		//map[mapkey] = (!k.includes('/') ? '@' : '') + 'http://' + App.HiveBind + ':' + z.Port;
@@ -167,7 +168,6 @@ App.LoadCell = function (cell) {
 		map[mapkey] = z.GotoURL || '@http://' + App.HiveBind + ':' + z.Port;
 		console.log('K = ' + k + '  ||  ' + 'KK = ' + kk + ' || ' + 'MAPKEY = ' + mapkey);
 	});
-
 	fs.mkdirSync('/hive/WEBGATE', { recursive: true }); fs.writeFileSync('/hive/WEBGATE/HIVE.MAP', yaml.dump(map));
 }
 
