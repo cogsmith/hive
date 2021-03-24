48482667a4664c5e886d9e895794c2d02865b218
DAC <dac@cogsmith.com>
2021-03-24 08:11:54 -0400

    .

--- app.js
+++ app.js
@@ -171,11 +171,14 @@ App.LoadCell = function (cell) {
 	fs.mkdirSync('/hive/WEBGATE', { recursive: true }); fs.writeFileSync('/hive/WEBGATE/HIVE.MAP', yaml.dump(map));
 }
 
+//
+
 App.GetHostSlug = function (host) { if (!host) { return host; } let slug = host.replace(/\./g, '_').toUpperCase(); let z = slug.split('_'); if (z.length >= 3) { slug = z.slice(-2).join('_') + '_' + z.slice(0, z.length - 2).reverse().join('_'); }; return slug; };
-//App.GetSlugHost = function (slug) { if (!slug) { return slug; } let host = slug.replace(/_/g, '.'); let z = slug.split('_'); if (z.length >= 2) { host = z.slice(2).reverse().join('.') + '.' + z.slice(0, 2).join('.'); }; return host.split('/')[0]; }
+//App.GetSlugHost = function (slug) { if (!slug) { return slug; } let host = slug.replace(/_/g, '.'); let z = slug.split('_'); if (z.length >= 2) { host = z.slice(2).reverse().join('.') + '.' + z.slice(0, 2).join('.'); }; return host; } // return host.split('/')[0]; };
 
-App.GetSlugHost = function (slug) { if (!slug) { return slug; } let host = slug.replace(/_/g, '.'); let z = slug.split('_'); if (z.length >= 2) { host = _.concat(z.slice(2).reverse(), z.slice(0, 2)).join('.'); }; return host.split('/')[0]; };
+App.GetSlugHost = function (slug) { if (!slug) { return slug; } let host = slug.replace(/_/g, '.'); let z = slug.split('_'); if (z.length >= 2) { host = _.concat(z.slice(2).reverse(), z.slice(0, 2)).join('.'); }; return host; } // return host.split('/')[0]; };
 
+//
 
 App.LoadSlug = function (slug) {
 	let slugpath = '/hive' + '/' + slug;
